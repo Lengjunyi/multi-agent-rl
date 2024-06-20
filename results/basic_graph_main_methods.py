e78674ffcda602b2486a9dc01e69b2c885fe4896
Alan Liang <al2520@ic.ac.uk>
2024-06-20 01:28:33 +0100

    Rename base experiments for consistency

--- results/basic_graph_main_methods.py
+++ results/basic_graph_main_methods.py
@@ -2,8 +2,15 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 files = [
-    ("results/SingleAgent.txt", "Single Agent"),
-    ("results\IQL.txt", "IQL"),
+    ("results/basic_SingleAgent.txt", "Single Agent"),
+
+    # Independent-Q-Learning
+    ("results/basic_IQL.txt", "IQL"),
+
+    # Advantage
+    ("results/basic_Advantage.txt", "Advantage"),
+
+
     # # "results\StateCompression.txt",
     # # ("results\IndependentLearnersLocalReward.txt", "IQL (Local)"),
     # ("results\AdvantageWithCompetitionDoubleQ.txt", "110%"),
@@ -15,7 +22,7 @@ files = [
     
     # ("input.txt", "default")
 ]
-
+avg = -1
 for file, name in files:
     with open(file, "r") as f:
         data = f.readlines()
@@ -31,11 +38,26 @@ for file, name in files:
 
         actions = re.compile(r"we got  tensor\((\d+\.\d+),")
         # smooth the data by taking mean of 10 episodes
-        # numbers = [np.mean(numbers[i:i+5]) for i in range(len(numbers)-5)]
+        numbers = [np.mean(numbers[i:i+5]) for i in range(len(numbers)-5)]
         print(numbers)
+        # numbers = numbers[:100]
         print(sum(numbers[-20:])/20, "average of last 20 episodes")
         print(len(numbers), "episodes trained")
         plt.plot(numbers, label=name)
+        if name == "Single Agent":
+            avg = sum(numbers[-20:])/20
+            # draw the baseline
+        if name == "Advantage":
+            avg_our = sum(numbers[-20:])/20
+
+# draw the baseline?
+plt.plot([avg]*140, label=f"Single-Agent Baseline (Avergage of Last 20) = {int(avg)}", linestyle="--")
+plt.plot([avg_our]*140, label=f"Our result (Average of Last 20) = {int(avg_our)}", linestyle="--")
+
+# no blank space left or right
+plt.xlim(0, 140)
+
+
 # plt.legend(files)
 plt.xlabel("Episodes")
 plt.ylabel("Total Reward")
@@ -56,13 +78,13 @@ for file, name in files:
         # extract the number if exists
         numbers = [
             float(pattern.search(line).group(1))/4000*100 for line in data if pattern.search(line)
-        ][:110]
+        ]#[:110]
 
         print(len(numbers))
 
         # smooth the data by taking median of 10 episodes
         # numbers = [np.median(numbers[i:i+10]) for i in range(len(numbers)-10)]
-        numbers = [np.mean(numbers[i:i+3]) for i in range(len(numbers)-3)]
+        # numbers = numbers[:100]
         print(numbers)
         print(sum(numbers[-20:])/20, "average of last 20 episodes")
         print(len(numbers), "episodes trained")
@@ -71,6 +93,6 @@ for file, name in files:
 plt.xlabel("Episodes")
 plt.ylabel("Percentage of Jobs Assigned Locally")
 plt.legend()
-plt.ylim(40, 100)
+plt.ylim(30, 100)
 
 plt.show()
